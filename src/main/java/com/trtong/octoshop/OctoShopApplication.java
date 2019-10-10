5745380af6cc32366c634f48211e90489b8c17dd
Trinh Tong <trt.tong@gmail.com>
2019-10-10 14:37:20 -0400

    barebones spring starter project

---
+++ src/main/java/com/trtong/octoshop/OctoShopApplication.java
@@ -0,0 +1,13 @@
+package com.trtong.octoshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OctoShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OctoShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trtong/octoshop/OctoShopApplicationTests.java
@@ -0,0 +1,16 @@
+package com.trtong.octoshop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OctoShopApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
